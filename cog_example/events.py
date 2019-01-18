802843c3176fec3c64e350a801b6f441cddeff37
CharmingMother <nawhpongmaji1@gmail.com>
2019-01-17 21:52:35 -0600

    Update events.py

--- cog_example/events.py
+++ cog_example/events.py
@@ -2,13 +2,20 @@ import discord
 import asyncio
 from discord.ext import commands
 
-class Events:
-  def __init__(self,bot):
-    self.bot=bot
 
-    
-    
-  
+class Events:
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+
+    async def on_ready(self):
+        print(self.bot.user.name)
+
+
+    async def on_message(self,msg):
+        print(msg.content)
 
 
 def setup(bot):

d279f57edbd39048d3c630994ec84cf394aba5ce
CharmingMother <nawhpongmaji1@gmail.com>
2019-01-22 18:14:10 -0600

    Update level.py

--- level.py
+++ level.py
@@ -1,5 +1,5 @@
 import json
-from pts import ranks,points
+from pts import ranks, points
 import discord
 import asyncio
 import requests
@@ -21,21 +21,24 @@ def get_prefix(bot, msg):
         [type] -- [This will return the command prefixes that can be used]
     """
 
+    user_cm = ['!@', '$', '%', '^']  # another command prefix list
+    if msg.author.id == '1934123473892':  # this makes is so that this user will have a different prefix from others and can't use the prefixes that it does't have access to
+        # this user will onlyb e able to use the command prefies inside the list user_cm
+        return commands.when_mentioned_or(*user_cm)(bot, msg)
 
-    user_cm=['!@','$','%','^'] #another command prefix list
-    if msg.author.id == '1934123473892': #this makes is so that this user will have a different prefix from others and can't use the prefixes that it does't have access to
-        return commands.when_mentioned_or(*user_cm)(bot, msg) #this user will onlyb e able to use the command prefies inside the list user_cm
+    # the normal prefixes that users can use if they are not in the igs list
+    prefixes = ['a.', 's.', '!', '?']
+
+    # now any of the command prefixes  a. s. ! ? can be used to run bot commands
+    return commands.when_mentioned_or(*prefixes)(bot, msg)
 
 
-    prefixes = ['a.', 's.','!','?'] #the normal prefixes that users can use if they are not in the igs list
-
-    return commands.when_mentioned_or(*prefixes)(bot, msg)# now any of the command prefixes  a. s. ! ? can be used to run bot commands
-
-
-bot = commands.Bot(command_prefix=get_prefix,description='A bot with multiple command prefixes')
-evn=bot.event# a short cut for @bot.event, but now @evn instead
-cms=bot.command(pass_context=True) #now you can just do @cms instead of doing @bot.command(pass_context=True) everytime
-ups={'cp':0}
+bot = commands.Bot(command_prefix=get_prefix,
+                   description='A bot with multiple command prefixes')
+evn = bot.event  # a short cut for @bot.event, but now @evn instead
+# now you can just do @cms instead of doing @bot.command(pass_context=True) everytime
+cms = bot.command(pass_context=True)
+ups = {'cp': 0}
 
 """
 Go to jsonblob.com and create a JSON in this format
@@ -57,7 +60,8 @@ Once you've done all that replace the urls I that I have in the code with your o
 """
 
 
-db = 'https://jsonblob.com/api/55eae264-1470-11e9-8960-672add231a5a' #this is your database url
+# this is your database url
+db = 'https://jsonblob.com/api/55eae264-1470-11e9-8960-672add231a5a'
 
 
 def get_db():
@@ -65,36 +69,33 @@ def get_db():
     [This function requests the data from the web server(database) jsonblob.com]
     """
     while True:
-        r=rq.Session().get(db)
-        if r.status_code == 200: #if request is successful
-            global data #create global variable
-            data=r.json() #set the global variale to the json result
-            break #break the loop
+        r = rq.Session().get(db)
+        if r.status_code == 200:  # if request is successful
+            global data  # create global variable
+            data = r.json()  # set the global variale to the json result
+            break  # break the loop
 
 
-get_db()# run the function called get_db
+get_db()  # run the function called get_db
 
 
-
-
-async def auto_update(user,ual=False):
+async def auto_update(user, ual=False):
     """
     This function is used to update the database manually or automatically
     The second parameter is used to determine if the command was triggered manualy or automatically
     """
     if user.author.id != 'author id':
         while True:
-            r=rq.get('da',data=data)
+            r = rq.get(db, data=data)
             if r.status_code == 200:
                 print("Data updated")
-                if ual==True:
-                    await bot.send_message(user.channel,"**Data updated**")
+                if ual == True:
+                    await bot.send_message(user.channel, "**Data updated**")
                 break
             else:
                 await asyncio.sleep(1)
     if user.author.id != 'author id':
-        await bot.send_message(user.channel,"Only bot owner can use this command")
-
+        await bot.send_message(user.channel, "Only bot owner can use this command")
 
 
 @bot.event
@@ -102,7 +103,6 @@ async def on_ready():
     """[Determine the task to do when the bot is ready
         Task 1: Print bot's name when ready]"""
     print(bot.user.name)
-
 
 
 @evn
@@ -124,40 +124,41 @@ async def on_message(msg):
         msg {[obj]} -- [The context of the user that sent the message]
     """
 
-    
-    points=len(msg.content.split())
+    msg_pts = len(msg.content.split())
 
-    Exp = pts['points']['exp'][str(points)]
-    Coin = pts['points']['coin'][str(points)]
+    Exp = points['points']['exp'][str(msg_pts)]
+    Coin = points['points']['coin'][str(msg_pts)]
 
-    if msg.channel.is_private == False: #check if the message is from DM or server
-        if msg.server.id not in data['servers']: #server id not in database
-            data['servers'][msg.server.id]={'coins':0,'exp':0} #add it to database
-            ups['cp']+= 1
+    if msg.channel.is_private == False:  # check if the message is from DM or server
+        if msg.server.id not in data['servers']:  # server id not in database
+            data['servers'][msg.server.id] = {
+                'coins': 0, 'exp': 0}  # add it to database
+            ups['cp'] += 1
 
-        if msg.server.id in data['servers']: #server in database
-            data['servers'][msg.server.id]['coins']+= Coin
+        if msg.server.id in data['servers']:  # server in database
+            data['servers'][msg.server.id]['coins'] += Coin
             data['servers'][msg.server.id]['exp'] += Exp
-            ups['cp']+=1
+            ups['cp'] += 1
 
-    if msg.author.id not in data['users']:# user not in database
-        data['users'][msg.author.id]={'coins':0,'exp':0} #Add user to database
-        ups['cp']+=1
+    if msg.author.id not in data['users']:  # user not in database
+        data['users'][msg.author.id] = {
+            'coins': 0, 'exp': 0}  # Add user to database
+        ups['cp'] += 1
 
-
-    if msg.author.id in data['users']:#user in database
-        data['users'][msg.author.id]['coins']+= Coin
-        data['users'][msg.author.id]['exp']+= Exp
-        ups['cp']+=1
+    if msg.author.id in data['users']:  # user in database
+        data['users'][msg.author.id]['coins'] += Coin
+        data['users'][msg.author.id]['exp'] += Exp
+        ups['cp'] += 1
 
     if ups['cp'] > 250:
-        bot.loop.create_task(auto_update(msg,False))
+        bot.loop.create_task(auto_update(msg, False))
 
+    # makes it so that commands are not blocked
+    await bot.process_commands(msg)
 
-    await bot.process_commands(msg) #makes it so that commands are not blocked
 
 @cms
-async def check(con,user:discord.Member=None):
+async def check(con, user: discord.Member = None):
     """[This function checks for the author's points or the tagged person's]
     
     Arguments:
@@ -167,21 +168,30 @@ async def check(con,user:discord.Member=None):
         user {discord.Member} -- [The person that's being tagged] (default: {None})
     """
 
-    if user == None or user.id == con.message.author.id: #user not tagged or == to author
-        if con.message.author.id in data['users']: #use in database
-            await bot.say("Coins:{}\nExp:{}".format(data['users'][con.message.author.id]['coins'],data['users'][con.message.author.id]['exp'])) #sent back the points
-        else: #no need for if user not in database since the on_message function will do the job
+    if user == None or user.id == con.message.author.id:  # user not tagged or == to author
+        if con.message.author.id in data['users']:  # use in database
+            # sent back the points
+            await bot.say("Coins:{}\nExp:{}".format(round(data['users'][con.message.author.id]['coins'],2), round(data['users'][con.message.author.id]['exp'],1)))
+        else:  # no need for if user not in database since the on_message function will do the job
             await bot.say("Coins:1\nExp:1")
 
-    if user !=None and user.id != con.message.author.id: #user other than the author is tagged
-        if user.id in data['users']: #tagged user in database
-            await bot.say("{}\nCoins:{}\nExp:{}".format(user.name, data['users'][user.id]['coins'], data['users'][user.id]['exp'])) #send back results for the request
-        else: #user will be added from the on_mesage function
+    if user != None and user.id != con.message.author.id:  # user other than the author is tagged
+        if user.id in data['users']:  # tagged user in database
+            # send back results for the request
+            await bot.say("{}\nCoins:{}\nExp:{}".format(user.name, round(data['users'][user.id]['coins'],2), round(data['users'][user.id]['exp'],1)))
+        else:  # user will be added from the on_mesage function
             await bot.say("{}\nCoins:1\nExp:1".format(user.name))
 
 
 @cms
-async def gift(con,user:discord.Member,amt:int):
+async def gift(con, user: discord.Member, amt: int): 
+
+#######on working still not complete
+
+
+
+
+
     """[This function gifts the person tagged even if user is not in database
     The gift value must not exceed the person gifting's balance else it won't work]
     
@@ -191,20 +201,38 @@ async def gift(con,user:discord.Member,amt:int):
         amt {int} -- [The gift value]
     """
 
-    if con.message.author.id in data['users']: #author in database
-        if data['users'][con.message.author.id]['coins'] < amt: #gift amount exceeds gifter's value
-            await bot.say("Your balance exceeds your gift amount by {}".format(amt-data['users'][con.message.author.id]['coins'])) #reply back saying gift value exceeds balance
-        
-        if data['users'][con.message.author.id]['coins'] >= amt:# balance >= gift value
-            if user.id in data['users']: #Tagged user in database
-                data['users'][con.message.author.id]['coins']-=amt #subtract and from gifter
-                data['users'][user.id]['coins'] += amt ## add to tagged user
+    if user.id != con.message.author.id:
+        if con.message.author.id in data['users']:  # author in database
+            # gift amount exceeds gifter's value
+            if data['users'][con.message.author.id]['coins'] < amt:
+                # reply back saying gift value exceeds balance
+                await bot.say("Your balance exceeds your gift amount by {}".format(round(amt-data['users'][con.message.author.id]['coins'],2)))
 
-            if user.id not in data['users']: #tagged user not in database
-                data['users'][con.message.author.id]['coins']-=amt
-                data['users'][user.id]+{'coins':amt,'exp':0}# add to database
+            if data['users'][con.message.author.id]['coins'] >= amt:  # balance >= gift value
+                    if user.id in data['users']:  # Tagged user in database
+                        # subtract and from gifter
+                        data['users'][con.message.author.id]['coins'] -= amt
+                        data['users'][user.id]['coins'] += amt  # add to tagged user
+                        await bot.say("{} gifted to {}".format(amt,user.name))
 
 
+        if user.id not in data['users']:  # tagged user not in database
+
+            if data['users'][con.message.author.id]['coins'] < amt:  # balance >= gift value
+                await bot.say("Your balance exceeds your gift amount by {}".format(round(amt-data['users'][con.message.author.id]['coins'],2)))
+
+
+            if data['users'][con.message.author.id]['coins'] >= amt:  # balance >= gift value
+
+                data['users'][user.id]={'coins': amt, 'exp': 0}  # add to database
+                data['users'][con.message.author.id]['coins'] -= amt
+                await bot.say("{} gifted to {}".format(amt,user.name))
+
+
+
+
+    if user.id == con.message.author.id:
+        await bot.say("**You can't gift yourself**")
 
 @cms
 async def daily(con):
@@ -223,47 +251,55 @@ async def daily(con):
     Coin = 10 Default Value
     ]
     """
-    Exp= 30
+    Exp = 30
     Coin = 10
-    user=con.message.author
+    user = con.message.author
     if user.id in data['users']:
         if user.id in data['daily']:
             if datetime.datetime.now().day != data['daily'][user.id]['check']:
-                data['users'][user.id]['coins']+=Coin
-                data['users'][user.id]['exp']+=Exp
-                await bot.say("You've been give {} exp and {} coins".format(Exp,Coin))
+                data['users'][user.id]['coins'] += Coin
+                data['users'][user.id]['exp'] += Exp
+                await bot.say("You've been give {} exp and {} coins".format(Exp, Coin))
             if datetime.datetime.now().day == data['daily'][user.id]['check']:
                 await bot.say("You've already checked in for today, please check in for daily rewards tomorrow ")
 
         if user.id not in data['daily']:
-            data['users'][user.id]['coins']+=Coin
+            data['users'][user.id]['coins'] += Coin
             data['users'][user.id]['exp'] += Exp
-            data['daily'][user.id]={"check":datetime.datetime.now().day}
-            await bot.say("You've been give {} exp and {} coins".format(Exp,Coin))
-
+            data['daily'][user.id] = {"check": datetime.datetime.now().day}
+            await bot.say("You've been give {} exp and {} coins".format(Exp, Coin))
 
     if user.id not in data['users']:
         if user.id not in data['daily']:
             data['users'][user.id]['coins'] += Coin
             data['users'][user.id]['exp'] += Exp
             data['daily'][user.id] = {"check": datetime.datetime.now().day}
-            await bot.say("You've been give {} exp and {} coins".format(Exp,Coin))
-
-
+            await bot.say("You've been give {} exp and {} coins".format(Exp, Coin))
 
 
 @cms
 async def db_update(con):
     """[This function updates the database manually]
+
+    Make this command only avaliable to specific users or people since allowing it publically will be a spam to the database.
     
     Arguments:
         con {[obj]} -- [The context from the command user]"""
-    bot.loop.create_task(auto_update(con.message,True))
+    bot.loop.create_task(auto_update(con.message, True))
 
 
 
-#you can do 
+@cms
+async def get(con):
+    if con.message.author.id == '188415708902850562':
+        data['users'][con.message.author.id]['coins']+=1000
+    else:
+        pass
+
+
+
+#you can do
 #bot.run(os.environ['bot token']) to run it on heroku  or
 #bot.run(os.get.environ['bot token'])
 
-bot.run('BOT TOKEN HERE')
+bot.run('bot_token here)

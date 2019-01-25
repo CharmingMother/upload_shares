800dd018a0a55f2a3bc1102f417b15f81af66d12
CharmingMother <nawhpongmaji1@gmail.com>
2019-01-25 14:44:37 -0600

    Update mode_mean_median_range.py

--- mode_mean_median_range.py
+++ mode_mean_median_range.py
@@ -9,7 +9,7 @@ data=[1,1,6,2,3,1,3]
 try:
     print("Mode:{}".format(mode(data)))
 except:
-    print("Mode: Error")
+    print("Mode: Error") #this means that there is no mode or there is more than 1 mode
 print("Mean is {}".format(mean(data)))
 print("median is {}".format(median(data)))
 print("Range is {}".format(max(data)-min(data)))

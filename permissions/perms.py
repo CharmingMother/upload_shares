238e73c1f2c587b6266dfff95ea09a2785e1205d
CharmingMother <nawhpongmaji1@gmail.com>
2019-02-26 16:12:09 -0600

    Update perms.py

--- permissions/perms.py
+++ permissions/perms.py
@@ -1,3 +1,26 @@
 import discord
 import asyncio
 from discord.ext import commands
+
+b0t = commands.Bot(command_prefix='.')
+
+
+@bot.event
+async def on_ready():
+  print(bot.user.name)
+  
+  
+
+@commands.has_permissions(manage_roles=True)
+@bot.command(pass_context=True)
+async def createRole(con,*,Name):
+  await bot.create_role(name=Name)
+  
+  
+  
+@commands.has_permissions(manage_messages=True,read_message_history=True)
+@bot.command(pass_context=True)
+async def clear(con,amt=50):
+  await bot.purge_from(channel=con.message.channel,limit=amt)
+
+bot.run('bot_token')
